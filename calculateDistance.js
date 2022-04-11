5921de94970c17405839e5a6c06f3f8e6fdd948a
ECAttorney <holster_safes_0@icloud.com>
2022-04-11 13:43:00 +0200

    1.2.0 added connected between HTML/ point/ cities

--- calculateDistance.js
+++ calculateDistance.js
@@ -1,27 +1,16 @@
 "use strict"
 
-var point = [7.595737,51.969508];
-
-var cities = [
-    [6.9570, 50.9367], //Köln
-    [4.9041, 52.3676], //Amsterdam
-    [9.4797, 51.3127], //Kassel
-    [2.1686, 41.3874], //Barcelona
-    [10.1815, 36.8065], //Tunis
-    [135.7681,35.0116], //Kyoto
-    [26.1025, 44.4268], //Bucharest
-    [15.4395, 47.0707], //Graz
-    [31.2357, 30.0444], //Kairo
-    [6.2603, 53.3498], //Dublin
-    [10.7522, 59.9139] //Oslo
-  ];
-
+//  Längen- bzw. Breitengrade von dem vorgegeben Punkt in jeweils einer Variable speichern
 var lat1 = point[1];
 var lon1 = point[0];
 
+// Array erstellen, welche so groß wie "cities" ist
 var distances= Array.apply(null, Array[cities.length])
+
+// Über diese Variable wird später die Auflistung auf der HTML-Seite ausgegeben
 var ausgabe = "";
 
+// Funktion, welche die Distanz zwischen 2 Punkten berechnet. Quelle: https://www.movable-type.co.uk/scripts/latlong.html
 function calculateDistance(){
 
     for(var i = 0; i < cities.length; i++){
@@ -29,8 +18,8 @@ function calculateDistance(){
         var lon2 = cities[i][0];
         var lat2 = cities[i][1];
     
-        const R = 6371e3; // metres
-        const φ1 = lat1 * Math.PI/180; // φ, λ in radians
+        const R = 6371e3; 
+        const φ1 = lat1 * Math.PI/180; 
         const φ2 = lat2 * Math.PI/180;
         const Δφ = (lat2-lat1) * Math.PI/180;
         const Δλ = (lon2-lon1) * Math.PI/180;
@@ -40,28 +29,24 @@ function calculateDistance(){
               Math.sin(Δλ/2) * Math.sin(Δλ/2);
         const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
     
-        const d = R * c; // in metres
+        const d = Math.round(R * c); // Hier wird die Distanz gerundet
     
-        distances[i] = d;
+        distances[i] = d; // Speicherung der Distanz in einem Array
 
     }
 
+// Hier wird das Array nun mittels Bubblesort der Entfernung nach aufsteigend sortiert
 bubbleSort(distances);
-
-for(var k = 0; k < cities.length; k++){
-
-    console.log("Die Stadt ist " + distances[k] + " Meter von dem Punkt entfernt");
-
-}
 
 for(var i = 0; i < distances.length; i++) {
 
-ausgabe = ausgabe + distances[i] + "<br />";
+ausgabe = ausgabe + distances[i] + " Meter <br />";
 
 }
 
 }
 
+// Funktion um ein Array mittels Bubblesort zu sortieren. Quelle: https://flexiple.com/bubble-sort-javascript
 let bubbleSort = (inputArr) => {
     let len = inputArr.length;
     for (let i = 0; i < len; i++) {
